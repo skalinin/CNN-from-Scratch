4450bd525c2571547f3040d58ec9b56ac69a7df7
Stanislav Kalinin <kalinin@sent.com>
2021-08-02 09:38:39 +0300

    feat: Add val loop

--- src/scripts/train_numpy_model.py
+++ src/scripts/train_numpy_model.py
@@ -11,25 +11,10 @@ from cnn.numpy_model import (
 
 class CnnFromScratch:
     def __init__(self):
-        self.conv1 = Conv2d(
-            stride=1,
-            in_channels=1,
-            out_channels=5,
-            kernel_size=(2, 2),
-        )
-        self.conv2 = Conv2d(
-            stride=2,
-            in_channels=5,
-            out_channels=20,
-            kernel_size=(3, 3),
-            padding=1,
-        )
-        self.max_pool = Maxpool2d(
-            kernel_size=(2, 2),
-            stride=2,
-            padding=1
-        )
-        self.fc1 = Linear(980, 2000)
+        self.conv1 = Conv2d(1, 5, 2, 3)
+        self.conv2 = Conv2d(5, 20, 3, 1, padding=1)
+        self.max_pool = Maxpool2d(2, 2, padding=1)
+        self.fc1 = Linear(500, 2000)
         self.fc2 = Linear(2000, 10)
         self.flatten = Flatten()
         self.relu = ReLU()
@@ -43,8 +28,8 @@ class CnnFromScratch:
         self.fc1.load_weights('fc_w_1', 'fc_b_1', load_path)
         self.fc2.load_weights('fc_w_2', 'fc_b_2', load_path)
 
-    def __call__(self, image, target):
-        x = self.conv1(image)
+    def __call__(self, x):
+        x = self.conv1(x)
         x = self.relu(x)
         x = self.max_pool(x)
         x = self.conv2(x)
@@ -56,17 +41,17 @@ class CnnFromScratch:
         x = self.softmax(x)
         return x
 
-    def backprop(self, x, learning_rate=0.01):
+    def backprop(self, x, lr=0.01):
         x = self.softmax.backprop(x)
-        x = self.fc2.backprop(x, learning_rate)
+        x = self.fc2.backprop(x, lr)
         x = self.sigmoid2.backprop(x)
-        x = self.fc1.backprop(x, learning_rate)
+        x = self.fc1.backprop(x, lr)
         x = self.flatten.vector2matrices(x)
         x = self.sigmoid1.backprop(x)
-        x = self.conv2.backprop(x, learning_rate)
+        x = self.conv2.backprop(x, lr)
         x = self.max_pool.backprop(x)
         x = self.relu.backprop(x)
-        x = self.conv1.backprop(x, learning_rate)
+        x = self.conv1.backprop(x, lr)
 
 
 def get_train_dataset():
@@ -84,47 +69,78 @@ def get_train_dataset():
         transform=transforms,
         target_transform=target_transform
     )
-    return train_dataset
+    test_dataset = torchvision.datasets.MNIST(
+        root='/workdir/data',
+        train=False,
+        download=True,
+        transform=transforms,
+        target_transform=target_transform
+    )
+    return train_dataset, test_dataset
+
+
+def train_loop(dataset, model, criterion, print_log_freq, lr):
+    loss_log = []
+    acc_log = []
+    start_time = time.time()
+    for idx, (image, target) in enumerate(dataset):
+        pred = model([image])
+        loss = criterion(target, pred)
+        x = criterion.backprop(target, pred)
+        model.backprop(x, lr=lr)
+
+        loss_log.append(loss.sum())
+        acc_log.append(pred.argmax() == target.argmax())
+        if idx % print_log_freq == 0:
+            loss_avg = sum(loss_log[-print_log_freq:])/print_log_freq
+            acc_avg = sum(acc_log[-print_log_freq:])/print_log_freq
+            loop_time = time.time() - start_time
+            start_time = time.time()
+            print(f'Train step {idx}, Loss: {loss_avg:.4f}, '
+                  f'Acc: {acc_avg:.4f}, loop time: {loop_time:.1f}')
+
+
+def val_loop(dataset, model, criterion):
+    loss_log = []
+    acc_log = []
+    start_time = time.time()
+    for idx, (image, target) in enumerate(dataset):
+        pred = model([image])
+        loss = criterion(target, pred)
+        loss_log.append(loss.sum())
+        acc_log.append(pred.argmax() == target.argmax())
+
+    loss_avg = sum(loss_log)/len(loss_log)
+    acc_avg = sum(acc_log)/len(acc_log)
+    loop_time = time.time() - start_time
+    print(f'Val step, Loss: {loss_avg:.4f}, '
+          f'Acc: {acc_avg:.4f}, loop time: {loop_time:.1f}')
 
 
 def main(args):
-    learning_rate = 0.01
-    train_dataset = get_train_dataset()
+    train_dataset, test_dataset = get_train_dataset()
     model = CnnFromScratch()
     if args.load_path:
         model.load_weights(args.load_path)
+        print('Numpy model weights were loaded')
+
     criterion = CrossEntropyLoss()
-
-    loss_log = []
-    acc_log = []
-    print_log_freq = args.print_log_freq
-    start_time = time.time()
-    for idx, (image, target) in enumerate(train_dataset):
-        predict = model([image], target)
-        loss = criterion(target, predict)
-
-        x = criterion.backprop(target, predict)
-        model.backprop(x, learning_rate)
-
-        loss_log.append(loss.sum())
-        acc_log.append(predict.argmax() == target.argmax())
-        if idx % print_log_freq == 0:
-            loss_avg = sum(loss_log)/len(loss_log)
-            acc_avg = sum(acc_log)/len(acc_log)
-            loss_log = []
-            acc_log = []
-            loop_time = time.time() - start_time
-            start_time = time.time()
-            print(f'Step {idx}, Loss: {loss_avg:.4f}, '
-                  f'Accyracy: {acc_avg:.4f}, loop time, sec: {loop_time:.1f}')
+    for epoch in range(args.num_epochs):
+        train_loop(train_dataset, model, criterion,
+                   args.print_log_freq, args.lr)
+        val_loop(test_dataset, model, criterion)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--print_log_freq', type=int, default=30,
+    parser.add_argument('--print_log_freq', type=int, default=1000,
                         help='Frequency of printing of training logs')
     parser.add_argument('--load_path', type=str, default='',
                         help='Path to model weights to start training with')
+    parser.add_argument('--num_epochs', type=int, default=1,
+                        help='Total number of epochs')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='learning rate')
     args = parser.parse_args()
 
     main(args)
--- src/scripts/train_torch_model.py
+++ src/scripts/train_torch_model.py
@@ -11,22 +11,23 @@ from cnn.transforms import PIL2numpy, Normalize, ToTensor
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 5, 2, 1)
-        self.conv2 = nn.Conv2d(5, 20, 3, 2, padding=1)
-        self.fc1 = nn.Linear(980, 2000)
+        self.conv1 = nn.Conv2d(1, 5, 2, 3)
+        self.conv2 = nn.Conv2d(5, 20, 3, 1, padding=1)
+        self.maxpool = nn.MaxPool2d(2, 2, padding=1)
+        self.fc1 = nn.Linear(500, 2000)
         self.fc2 = nn.Linear(2000, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, 2, stride=2, padding=1)
+        x = self.maxpool(x)
         x = self.conv2(x)
         x = torch.sigmoid(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = torch.sigmoid(x)
         x = self.fc2(x)
-        return x  # log_softmax already in F.cross_entropy
+        return x  # log_softmax is in CrossEntropyLoss
 
 
 def get_train_loader():
@@ -41,51 +42,87 @@ def get_train_loader():
         download=True,
         transform=transforms
     )
+    test_dataset = torchvision.datasets.MNIST(
+        root='/workdir/data',
+        train=False,
+        download=True,
+        transform=transforms
+    )
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1)
-    return train_loader
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1)
+    return train_loader, test_loader
+
+
+def train_loop(dataset, model, criterion, optimizer, print_log_freq):
+    loss_log = []
+    acc_log = []
+    start_time = time.time()
+    model.train()
+    for idx, (image, target) in enumerate(dataset):
+        image = image.unsqueeze(0)  # Add channel to make input 4D
+        optimizer.zero_grad()
+        pred = model(image)
+        loss = criterion(pred, target)
+        loss.backward()
+        optimizer.step()
+
+        loss_log.append(loss.item())
+        acc_log.append(pred.argmax().item() == target.item())
+        if idx % print_log_freq == 0:
+            loss_avg = sum(loss_log[-print_log_freq:])/print_log_freq
+            acc_avg = sum(acc_log[-print_log_freq:])/print_log_freq
+            loop_time = time.time() - start_time
+            start_time = time.time()
+            print(f'Train step {idx}, Loss: {loss_avg:.4f}, '
+                  f'Acc: {acc_avg:.4f}, loop time: {loop_time:.1f}')
+
+
+def val_loop(dataset, model, criterion):
+    loss_log = []
+    acc_log = []
+    start_time = time.time()
+    model.eval()
+    for idx, (image, target) in enumerate(dataset):
+        image = image.unsqueeze(0)  # Add channel to make input 4D
+        with torch.no_grad():
+            pred = model(image)
+            loss = criterion(pred, target)
+        loss_log.append(loss.item())
+        acc_log.append(pred.argmax().item() == target.item())
+
+    loss_avg = sum(loss_log)/len(loss_log)
+    acc_avg = sum(acc_log)/len(acc_log)
+    loop_time = time.time() - start_time
+    print(f'Val step, Loss: {loss_avg:.4f}, '
+          f'Acc: {acc_avg:.4f}, loop time: {loop_time:.1f}')
 
 
 def main(args):
-    train_loader = get_train_loader()
+    train_loader, test_loader = get_train_loader()
     model = Net()
     if args.load_path:
         states = torch.load(args.load_path)
         model.load_state_dict(states)
-        print('Model weights were loaded')
-    model.train()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+        print('Torch model weights were loaded')
 
-    loss_log = []
-    acc_log = []
-    print_log_freq = args.print_log_freq
-    start_time = time.time()
-    for idx, (image, target) in enumerate(train_loader):
-        image = image.unsqueeze(0)  # Add channel to make input 4D
-        optimizer.zero_grad()
-        output = model(image)
-        loss = F.cross_entropy(output, target)
-        loss.backward()
-        optimizer.step()
-
-        loss_log.append(loss)
-        acc_log.append(output.argmax().item() == target.item())
-        if idx % print_log_freq == 0:
-            loss_avg = sum(loss_log)/len(loss_log)
-            acc_avg = sum(acc_log)/len(acc_log)
-            loss_log = []
-            acc_log = []
-            loop_time = time.time() - start_time
-            start_time = time.time()
-            print(f'Step {idx}, Loss: {loss_avg:.4f}, '
-                  f'Accyracy: {acc_avg:.4f}, loop time, sec: {loop_time:.1f}')
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    criterion = nn.CrossEntropyLoss()
+    for epoch in range(args.num_epochs):
+        train_loop(train_loader, model, criterion,
+                   optimizer, args.print_log_freq)
+        val_loop(test_loader, model, criterion)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--print_log_freq', type=int, default=30,
+    parser.add_argument('--print_log_freq', type=int, default=1000,
                         help='Frequency of printing of training logs')
     parser.add_argument('--load_path', type=str, default='',
                         help='Path to model weights to start training with')
+    parser.add_argument('--num_epochs', type=int, default=1,
+                        help='Total number of epochs')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='learning rate')
     args = parser.parse_args()
 
     main(args)
